a0ef22bf5e2e426e37b69a03774bdf69c10f54f3
mustaphaas <63932124+mustaphaas@users.noreply.github.com>
2026-08-12 10:11:58 +0100

    Add files via upload

--- Atlasgridinspection-20-main/client/App.tsx
+++ Atlasgridinspection-20-main/client/App.tsx
@@ -1,16 +1,18 @@
 import "./global.css";
 import "./rea-redesign.css";
+import "./modernized.css";
 
 import { Toaster } from "@/components/ui/toaster";
-import { createRoot } from "react-dom/client";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { AtlasGridProvider } from "@/context/AtlasGridContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import FieldOfficer from "./pages/FieldOfficer";
-import Login from "./pages/Login";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ConsultantAdmin from "./pages/ConsultantAdmin";
+import FieldOfficer from "./pages/FieldOfficer";
+import Index from "./pages/Index";
+import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,18 +20,20 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/view" element={<Index />} />
-          <Route path="/field-officer" element={<FieldOfficer />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/consultant-admin" element={<ConsultantAdmin />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <AtlasGridProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/view" element={<Index />} />
+            <Route path="/field-officer" element={<FieldOfficer />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/consultant-admin" element={<ConsultantAdmin />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AtlasGridProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
